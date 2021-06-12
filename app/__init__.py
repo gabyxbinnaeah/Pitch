476af4b9110fd71725ae8d8cacfaf7b6cef7a6f1
gabriel odhiambo <gabyxbinnaeh4@gmail.com>
2021-06-12 17:13:07 +0300

    import configuration

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,9 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap 
+from config import config_options
 
 bootstrap=Bootstrap()
 #initializing app
 app=Flask(__name__)
 
-bootstrap.init_app(app)
+bootstrap.init_app(app) 
