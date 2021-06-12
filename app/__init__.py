7c086d4e3e3f0293cf13fe14fd971afc6b58be3d
gabriel odhiambo <gabyxbinnaeh4@gmail.com>
2021-06-12 17:21:35 +0300

    adds configuration options

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,16 @@ from flask import Flask
 from flask_bootstrap import Bootstrap 
 from config import config_options
 
+
 bootstrap=Bootstrap()
 #initializing app
-app=Flask(__name__)
 
-bootstrap.init_app(app) 
+def creat_app(configname):
+    app=Flask(__name__)
+
+    bootstrap.init_app(app) 
+
+    #configuration options
+    app.config.from_object(config_options[configname])
+
+    from .main import views,error,forms 
