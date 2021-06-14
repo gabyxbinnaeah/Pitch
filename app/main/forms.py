19b89161e33a2bd31e96ee5f97e3b3982f277194
gabriel odhiambo <gabyxbinnaeh4@gmail.com>
2021-06-14 19:10:52 +0300

    creates input fields for creating different pitches

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,10 +1,21 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,TextAreaField,SubmitField
-from wtforms.validators import Required 
+from wtforms import StringField,PasswordField,SubmitField,BooleanField,SubmitField,TextAreaField,RadioField
+from wtforms.validators import Required,Email,EqualTo
+from wtforms import ValidationError 
 
-class PitchCommentForm(FlaskForm):
+class PitForm(FlaskForm):
     title=StringField('Pitch title', validators=[Required()])
-    content=TextAreaField('Pitch Comment', validators=[Required()])
+    description=TextAreaField("What would you like to pitch in one minute?", validators=[Required()])
+    category=RadioField('Label', choices=[('businesspitch','businesspitch'),('interviewpitch','interviewpitch'),('politicalpitch','politicalpitch'),('religiouspitch','religiouspitch')],validators=[Required()])
     submit=SubmitField('Submit')
 
     
+class PitchCommentsForm(FlaskForm):
+    description=TextAreaField('Add comment',validators=[Required()]) 
+    Submit=SubmitField() 
+
+class UpvotesForm(FlaskForm):
+    Submit=SubmitField()
+
+class DownvotesForm(FlaskForm):
+    Submit=SubmitField() 
