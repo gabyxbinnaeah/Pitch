a09b7a72bffbb2c83425a4c21e5604b1680cf37c
gabriel odhiambo <gabyxbinnaeh4@gmail.com>
2021-06-14 15:56:11 +0300

    creates blueprint in the auth subdirectory

--- app/__init__.py
+++ app/__init__.py
@@ -36,7 +36,7 @@ def create_app(config_name):
         #register blueprint
     app.register_blueprint(main_blueprint) 
 
-        # Regestering the auth bluprint
+        # Registering the auth bluprint
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
