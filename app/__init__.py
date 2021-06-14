bb8b659b55e55ee05f92a8b4b48d563cf7ebc13d
gabriel odhiambo <gabyxbinnaeh4@gmail.com>
2021-06-14 15:37:50 +0300

    Instance of LoginManger and using its methods

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,19 @@ from flask import Flask
 from flask_bootstrap import Bootstrap 
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_simplemde import SimpleMDE
 
 
 
-
-bootstrap=Bootstrap()
-db=SQLAlchemy()
+# Instances of flask extensions
+# Instance of LoginManger and using its methods
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+simple = SimpleMDE()
 #initializing app
 
 def create_app(configname):
