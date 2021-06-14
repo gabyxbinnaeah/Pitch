0db5c0acdf04bef4557ee1134d86089e16afeba2
gabriel odhiambo <gabyxbinnaeh4@gmail.com>
2021-06-14 18:47:00 +0300

    creates an authenticated route logout that call the flask login'as logout function

--- app/auth/views.py
+++ app/auth/views.py
@@ -31,4 +31,9 @@ def login():
     title = "Pitch Login"
     return render_template('auth/login.html',login_form = login_form,title=title) 
 
-    
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index")) 
