58db76ad9db4b48672afa3eb3bbfc2396eab6832
gabriel odhiambo <gabyxbinnaeh4@gmail.com>
2021-06-14 15:47:15 +0300

    # registers the auth bluprint

--- app/__init__.py
+++ app/__init__.py
@@ -17,18 +17,28 @@ db = SQLAlchemy()
 simple = SimpleMDE()
 #initializing app
 
-def create_app(configname):
+def create_app(config_name):
     app=Flask(__name__)
+    
+    simple.init_app(app)
 
+       # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+      
+       # Initialising flask extensions
     bootstrap.init_app(app)
     db.init_app(app) 
-
-    #configuration options
-    app.config.from_object(config_options[configname])
-    
+    login_manager.init_app(app)
+        #configuration options
+    app.config.from_object(config_options[config_name])
+        # Regestering the main blueprint
     from .main import main as main_blueprint 
-     #register blueprint
+        #register blueprint
     app.register_blueprint(main_blueprint) 
+
+        # Regestering the auth bluprint
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
     return app    
 
